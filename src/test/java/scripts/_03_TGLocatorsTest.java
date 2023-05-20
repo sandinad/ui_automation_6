0f5c3bd3fd35de9d799e36bf025db929c57980d4
akinkayagithub <akinkaya.b@gmail.com>
2023-05-20 11:48:04 -0500

    added locator id

--- src/test/java/scripts/_03_TGLocatorsTest.java
+++ src/test/java/scripts/_03_TGLocatorsTest.java
@@ -1,7 +1,9 @@
 package scripts;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
@@ -10,11 +12,11 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-public class _02_AppleTest {
+public class _03_TGLocatorsTest {
     /*
-    Go to https://www.apple.com/
-    Validate its title is apple
-    Validate its URL is https://www.apple.com/
+    Test case 1:
+    Go to "https://techglobal-training.com/frontend/locators"
+    Validate the "Kiwi" paragraph
      */
 
     public static WebDriver driver; // declaration
@@ -28,7 +30,7 @@ public class _02_AppleTest {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
-        driver.get("https://www.apple.com/");
+        driver.get("https://techglobal-training.com/frontend/locators");
     }
 
     @AfterMethod
@@ -38,8 +40,17 @@ public class _02_AppleTest {
     }
 
     @Test
-    public void validateTitleAndURL()  {
-        Assert.assertEquals(driver.getTitle(), "Apple");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://www.apple.com/");
+    public void validateKiwiParagraph(){
+        WebElement kiwi_paragraph = driver.findElement(By.id("item_kiwi"));
+
+        Assert.assertTrue(kiwi_paragraph.isDisplayed());
+        Assert.assertEquals(kiwi_paragraph.getText(), "Kiwi");
     }
+
+
+     /*
+    Test case 2:
+    Go to "https://techglobal-training.com/frontend/locators"
+    Validate the "Orange" paragraph
+     */
 }
