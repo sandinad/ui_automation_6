4fa167594935e6f45045395850e5b880fe6f8d90
buraksimsekgit <simsek.bl@gmail.com>
2023-05-27 14:04:31 -0500

    handling alerts

--- src/test/java/scripts/_11_TGDropdownsTest.java
+++ src/test/java/scripts/_11_TGDropdownsTest.java
@@ -1,9 +1,13 @@
 package scripts;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.DropdownHandler;
+
+import java.util.List;
 
 public class _11_TGDropdownsTest extends Base {
 
@@ -16,15 +20,53 @@ public class _11_TGDropdownsTest extends Base {
     @Test
     public void productDropdownTest(){
 
-        Select select = new Select(driver.findElement(By.id("product_dropdown")));
-
-        select.selectByVisibleText("iPhone 14 Pro Max");
+        WebElement dropdown = driver.findElement(By.id("product_dropdown"));
+        DropdownHandler.selectByVisibleText(dropdown, "iPhone 14 Pro Max");
     }
 
     @Test
     public void productColorTest(){
-        Select select = new Select(driver.findElement(By.id("color_dropdown")));
+        WebElement dropdown = driver.findElement(By.id("color_dropdown"));
+        DropdownHandler.selectByIndex(dropdown, 2);
+    }
 
-        select.selectByIndex(2);
+    @Test
+    public void productDeliveryTest(){
+        WebElement dropdown = driver.findElement(By.id("shipment_dropdown"));
+        List<WebElement> dropdownOption = driver.findElements(By.cssSelector("#shipment_dropdown span"));
+
+       DropdownHandler.clickOnDropdownOption(dropdown, dropdownOption, "Delivery");
+    }
+
+    /**
+     * Test Case: Validate Dropdowns Functionality on TechGlobal Training Page
+     * Go to https://techglobal-training.com/frontend/
+     * Click on the "Dropdowns" card
+     * Select the "MacBook Pro 13" option from the "Product" dropdown.
+     * Select the "Green" option from the "Color" dropdown.
+     * Open the "Shipping" dropdown and click on the "Delivery" option.
+     * Click on the "Submit" button.
+     * Validate result message displays "Your Green MacBook Pro 13 will be delivered to you."
+     */
+
+    @Test
+    public void dropdownTestCase(){
+        WebElement productDropdown = driver.findElement(By.id("product_dropdown"));
+        DropdownHandler.selectByVisibleText(productDropdown, "MacBook Pro 13");
+
+        WebElement colorDropdown = driver.findElement(By.id("color_dropdown"));
+        DropdownHandler.selectByVisibleText(colorDropdown, "Green");
+
+        WebElement deliveryDropdown = driver.findElement(By.id("shipment_dropdown"));
+        deliveryDropdown.click();
+
+        List<WebElement> deliveryDropdownOptions = driver.findElements(By.cssSelector("#shipment_dropdown span"));
+        deliveryDropdownOptions.get(0).click();
+
+        driver.findElement(By.id("submit")).click();
+        String actualText = driver.findElement(By.id("result")).getText();
+
+        Assert.assertEquals(actualText, "Your Green MacBook Pro 13 will be delivered to you.");
     }
 }
+
---
+++ src/test/java/scripts/_12_TGAlertsTest.java
@@ -0,0 +1,90 @@
+package scripts;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import utils.AlertHandler;
+import utils.Driver;
+import utils.Waiter;
+
+public class _12_TGAlertsTest extends Base {
+
+    @BeforeMethod
+    public void setPage(){
+        driver.get("https://techglobal-training.com/frontend");
+        driver.findElement(By.id("card-8")).click();
+    }
+
+    /**
+     * WARNING ALERT
+     * Go to https://techglobal-training.com/frontend/
+     * Click on the "Alerts" card
+     * Click on the "Warning alert" button
+     * Validate the alert text equals "You are on TechGlobal Training application."
+     * Click on the "OK" button on the alert
+     * Validate the result message equals "You accepted warning by clicking OK."
+     */
+    @Test
+    public void warningAlert(){
+        WebElement warningAlert = driver.findElement(By.id("warning_alert"));
+        warningAlert.click();
+
+
+        Assert.assertEquals(AlertHandler.getAlertText(), "You are on TechGlobal Training application.");
+
+        AlertHandler.acceptAlert();
+
+        WebElement resultText = driver.findElement(By.id("action"));
+        Assert.assertEquals(resultText.getText(), "You accepted warning by clicking OK.");
+    }
+
+    /**
+     * Go to https://techglobal-training.com/frontend/
+     * Click on the "Alerts" card
+     * Click on the "Confirmation alert" button
+     * Validate the alert text equals "Would you like to stay on TechGlobal Training application?"
+     * Click on the "Cancel" button on the alert
+     * Validate the result message equals "You rejected the alert by clicking Cancel."
+     * Click on the "Confirmation alert" button again
+     * Click on the "OK" button on the alert
+     * Validate the result message equals "You confirmed the alert by clicking OK."
+     */
+
+    @Test
+    public void confirmationAlert(){
+        WebElement confirmationAlert = driver.findElement(By.id("confirmation_alert"));
+        confirmationAlert.click();
+
+        Assert.assertEquals(AlertHandler.getAlertText(), "Would you like to stay on TechGlobal Training application?");
+
+        AlertHandler.dismissAlert();
+
+        WebElement resultText = driver.findElement(By.id("action"));
+        Assert.assertEquals(resultText.getText(), "You rejected the alert by clicking Cancel.");
+
+        confirmationAlert.click();
+        AlertHandler.acceptAlert();
+        Assert.assertEquals(resultText.getText(), "You confirmed the alert by clicking OK.");
+
+    }
+
+    /**
+     * PROMPT ALERT
+     * Go to https://techglobal-training.com/frontend/
+     * Click on the "Alerts" card
+     * Click on the "Prompt alert" button
+     * Validate the alert text equals "What would you like to say to TechGlobal?"
+     * Click on the "Cancel" button on the alert
+     * Validate the result message equals "You rejected the alert by clicking Cancel."
+     * Click on the "Prompt alert" button again
+     * Click on the "OK" button on the alert
+     * Validate the result message equals "You entered "" in the alert and clicked OK."
+     * Click on the "Prompt alert" button again
+     * Enter "Hello" to alert the input box
+     * Click on the "OK" button on the alert
+     * Validate the result message equals "You entered "Hello" in the alert and clicked OK."
+     */
+}
