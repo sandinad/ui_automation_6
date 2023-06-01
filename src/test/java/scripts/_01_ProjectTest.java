8238afac3e2011b0a8cb5153ed416599337fe13b
Tech Global <tgschool01@gmail.com>
2023-06-01 09:57:26 -0700

    project01

--- src/test/java/scripts/_01_ProjectTest.java
+++ src/test/java/scripts/_01_ProjectTest.java
@@ -11,13 +11,44 @@ import utils.Waiter;
 import java.util.List;
 
 public class _01_ProjectTest extends Base{
-    /*
-    COMPLETE PROJECT01 HERE
-     */
 
     @BeforeMethod
     public void setPage(){
         driver.get("https://techglobal-training.com/frontend/project-1");
+    }
+
+    @Test(priority = 1, description = "Test Case 01 - Validate the Contact Us information")
+    public void validateTheContactUsInformation(){
+        WebElement contactUsHeading = driver.findElement(By.cssSelector(".mb-5>h1"));
+        WebElement contactUsAddress = driver.findElement(By.id("address"));
+        WebElement contactUsEmail = driver.findElement(By.id("email"));
+        WebElement contactUsPhoneNumber = driver.findElement(By.id("phone-number"));
+
+        Assert.assertTrue(contactUsHeading.isDisplayed());
+        Assert.assertEquals(contactUsHeading.getText(), "Contact Us");
+
+        Assert.assertTrue(contactUsAddress.isDisplayed());
+        Assert.assertEquals(contactUsAddress.getText(), "2860 S River Rd Suite 350, Des Plaines IL 60018");
+
+        Assert.assertTrue(contactUsEmail.isDisplayed());
+        Assert.assertEquals(contactUsEmail.getText(), "info@techglobalschool.com");
+
+        Assert.assertTrue(contactUsPhoneNumber.isDisplayed());
+        Assert.assertEquals(contactUsPhoneNumber.getText(), "(773) 257-3010");
+    }
+
+    @Test(priority = 2, description = "Test Case 02 - Validate the Full name input box")
+    public void validateTheFullNameInputBox(){
+        WebElement nameInputBox = driver.findElement(By.xpath("//label[@for='name']/..//input"));
+        WebElement nameInputBoxLabel = driver.findElement(By.xpath("//label[@for='name']"));
+
+        Assert.assertTrue(nameInputBox.isDisplayed());
+        Assert.assertTrue(nameInputBox.isEnabled());
+        Assert.assertEquals(nameInputBox.getAttribute("required"), "true");
+        Assert.assertEquals(nameInputBox.getAttribute("placeholder"), "Enter your name");
+
+        Assert.assertTrue(nameInputBoxLabel.isDisplayed());
+        Assert.assertEquals(nameInputBoxLabel.getText(), "Full name *");
     }
 
     @Test(priority = 3, description = "Test Case 03 - Validate the Gender radio button")
@@ -53,6 +84,61 @@ public class _01_ProjectTest extends Base{
         Waiter.pause(5);
     }
 
+    @Test(priority = 4, description = "Test Case 04 - Validate the Address input box")
+    public void validateTheAddressInputBox(){
+        WebElement addressInputBox = driver.findElement(By.xpath("//label[text()='Address']/..//input"));
+        WebElement addressInputBoxLabel = driver.findElement(By.xpath("//label[text()='Address']"));
+
+        Assert.assertTrue(addressInputBox.isDisplayed());
+        Assert.assertTrue(addressInputBox.isEnabled());
+        Assert.assertNotEquals(addressInputBox.getAttribute("required"), "true");
+        Assert.assertEquals(addressInputBox.getAttribute("placeholder"), "Enter your address");
+
+        Assert.assertTrue(addressInputBoxLabel.isDisplayed());
+        Assert.assertEquals(addressInputBoxLabel.getText(), "Address");
+    }
+
+    @Test(priority = 5, description = "Test Case 05 - Validate the Email input box")
+    public void validateTheEmailInputBox(){
+        WebElement emailInputBox = driver.findElement(By.cssSelector("input[type='email']"));
+        WebElement emailInputBoxLabel = driver.findElement(By.xpath("//label[text()='Email *']"));
+
+        Assert.assertTrue(emailInputBox.isDisplayed());
+        Assert.assertTrue(emailInputBox.isEnabled());
+        Assert.assertEquals(emailInputBox.getAttribute("required"), "true");
+        Assert.assertEquals(emailInputBox.getAttribute("placeholder"), "Enter your email");
+
+        Assert.assertTrue(emailInputBoxLabel.isDisplayed());
+        Assert.assertEquals(emailInputBoxLabel.getText(), "Email *");
+    }
+
+    @Test(priority = 6, description = "Test Case 06 - Validate the Phone input box")
+    public void validateThePhoneInputBox(){
+        WebElement phoneInputBox = driver.findElement(By.cssSelector("input[type='phone']"));
+        WebElement phoneInputBoxLabel = driver.findElement(By.xpath("//label[text()='Phone']"));
+
+        Assert.assertTrue(phoneInputBox.isDisplayed());
+        Assert.assertTrue(phoneInputBox.isEnabled());
+        Assert.assertNotEquals(phoneInputBox.getAttribute("required"), "true");
+        Assert.assertEquals(phoneInputBox.getAttribute("placeholder"), "Enter your phone number");
+
+        Assert.assertTrue(phoneInputBoxLabel.isDisplayed());
+        Assert.assertEquals(phoneInputBoxLabel.getText(), "Phone");
+    }
+
+    @Test(priority = 7, description = "Test Case 07 - Validate the Message text area")
+    public void validateTheMessageTextArea(){
+        WebElement messageTextArea = driver.findElement(By.tagName("textarea"));
+        WebElement messageTextAreaLabel = driver.findElement(By.xpath("//label[text()='Message']"));
+
+        Assert.assertTrue(messageTextArea.isDisplayed());
+        Assert.assertTrue(messageTextArea.isEnabled());
+        Assert.assertNotEquals(messageTextArea.getAttribute("required"), "true");
+        Assert.assertEquals(messageTextArea.getAttribute("placeholder"), "Type your message here...");
+
+        Assert.assertTrue(messageTextAreaLabel.isDisplayed());
+        Assert.assertEquals(messageTextAreaLabel.getText(), "Message");
+    }
 
     @Test(priority = 8, description = "Test Case 08 - Validate the Consent checkbox")
     public void validateTheConsentCheckbox(){
@@ -73,6 +159,15 @@ public class _01_ProjectTest extends Base{
         consentCheckBox.click();
         Waiter.pause(2);
         Assert.assertFalse(consentCheckBox.isSelected());
+    }
+
+    @Test(priority = 9, description = "Test Case 09 - Validate the SUBMIT button")
+    public void validateTheSubmitButton(){
+        WebElement submitButton = driver.findElement(By.cssSelector(".is-link"));
+
+        Assert.assertTrue(submitButton.isDisplayed());
+        Assert.assertTrue(submitButton.isEnabled());
+        Assert.assertEquals(submitButton.getText(), "SUBMIT");
     }
 
     @Test(priority = 10, description = "Test Case 10 - Validate the form submission")
@@ -101,7 +196,5 @@ public class _01_ProjectTest extends Base{
 
         Assert.assertTrue(confirmationMessage.isDisplayed());
         Assert.assertEquals(confirmationMessage.getText(), "Thanks for submitting!");
-
-        Waiter.pause(30);
     }
 }
