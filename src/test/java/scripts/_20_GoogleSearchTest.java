f7f719507f9c50046fd3af0ee43f89ae9d78c3d9
akinkayagithub <akinkaya.b@gmail.com>
2023-06-11 11:41:06 -0500

    added dataprovider

--- src/test/java/scripts/_20_GoogleSearchTest.java
+++ src/test/java/scripts/_20_GoogleSearchTest.java
@@ -2,6 +2,7 @@ package scripts;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.GoogleSearchPage;
 import pages.GoogleSearchResultsPage;
@@ -23,5 +24,20 @@ public class _20_GoogleSearchTest extends Base {
         String resultTag = googleSearchResultsPage.resultTag.getText(); // About 1,010,000,000 results (0.60 seconds)
 
         Assert.assertTrue(Integer.parseInt(resultTag.replaceAll("[^0-9]", "").substring(0, 1)) > 0);
+    }
+
+    @Test(priority = 2, description = "Validate the Google Search", dataProvider = "searchKeys")
+    public void validateTheSearch(String key){
+        googleSearchPage.search(key);
+
+        Assert.assertTrue(googleSearchResultsPage.resultTag.isDisplayed());
+        String resultTag = googleSearchResultsPage.resultTag.getText();
+
+        Assert.assertTrue(Integer.parseInt(resultTag.replaceAll("[^0-9]", "").substring(0, 1)) > 0);
+    }
+
+    @DataProvider
+    public Object[] searchKeys(){
+        return new Object[]{"Tesla", "Microsoft", "Apple", "Oracle"};
     }
 }
